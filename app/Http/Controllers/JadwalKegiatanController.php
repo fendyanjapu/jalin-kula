11eda6223b8d06d7bd6ea18389e740f2ff52ef52
Noor Rahmat Effendy <noorrahmateffendy@Noors-iMac.local>
2024-04-16 16:21:35 +0800

    Add Jadwal Kegiatan Model and Controller

---
+++ app/Http/Controllers/JadwalKegiatanController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JadwalKegiatan;
+use Illuminate\Http\Request;
+
+class JadwalKegiatanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JadwalKegiatan $jadwalKegiatan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JadwalKegiatan $jadwalKegiatan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JadwalKegiatan $jadwalKegiatan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JadwalKegiatan $jadwalKegiatan)
+    {
+        //
+    }
+}
